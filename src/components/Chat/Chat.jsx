c9924010903de3ccdd2413834ac00f5989f3fd84
raphlev <raphleveque@hotmail.fr>
2021-06-04 23:00:32 +0200

    Add display none to ChatEngine component to fix hideUI which has no effect

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -21,49 +21,52 @@ export const Chat = () => {
     console.log('Selected Chat: ', selectedChat);
   }, [selectedChat]);
 
+  // hideUI on ChatEngine has no effect
   return (
     <>
       {!!chatConfig && (
-        <ChatEngine
-          hideUI={true}
-          userName={chatConfig.userName}
-          projectID={chatConfig.projectID}
-          userSecret={chatConfig.userSecret}
-          onConnect={() => {
-            getChats(chatConfig, setMyChats);
-          }}
-          onNewChat={chat => {
-            if (chat.admin.username === chatConfig.userName) {
-              selectChatClick(chat);
-            }
-            setMyChats([...myChats, chat].sort((a, b) => a.id - b.id));
-          }}
-          onDeleteChat={chat => {
-            if (selectedChat?.id === chat.id) {
-              setSelectedChat(null);
-            }
-            setMyChats(
-              myChats.filter(c => c.id !== chat.id).sort((a, b) => a.id - b.id),
-            );
-          }}
-          onNewMessage={(chatId, message) => {
-            if (selectedChat && chatId === selectedChat.id) {
-              setSelectedChat({
-                ...selectedChat,
-                messages: [...selectedChat.messages, message],
-              });
-            }
-            const chatThatMessageBelongsTo = myChats.find(c => c.id === chatId);
-            const filteredChats = myChats.filter(c => c.id !== chatId);
-            const updatedChat = {
-              ...chatThatMessageBelongsTo,
-              last_message: message,
-            };
-            setMyChats(
-              [updatedChat, ...filteredChats].sort((a, b) => a.id - b.id),
-            );
-          }}
-        />
+        <div style={{ display: 'none' }}>
+          <ChatEngine
+            hideUI={true}
+            userName={chatConfig.userName}
+            projectID={chatConfig.projectID}
+            userSecret={chatConfig.userSecret}
+            onConnect={() => {
+              getChats(chatConfig, setMyChats);
+            }}
+            onNewChat={chat => {
+              if (chat.admin.username === chatConfig.userName) {
+                selectChatClick(chat);
+              }
+              setMyChats([...myChats, chat].sort((a, b) => a.id - b.id));
+            }}
+            onDeleteChat={chat => {
+              if (selectedChat?.id === chat.id) {
+                setSelectedChat(null);
+              }
+              setMyChats(
+                myChats.filter(c => c.id !== chat.id).sort((a, b) => a.id - b.id),
+              );
+            }}
+            onNewMessage={(chatId, message) => {
+              if (selectedChat && chatId === selectedChat.id) {
+                setSelectedChat({
+                  ...selectedChat,
+                  messages: [...selectedChat.messages, message],
+                });
+              }
+              const chatThatMessageBelongsTo = myChats.find(c => c.id === chatId);
+              const filteredChats = myChats.filter(c => c.id !== chatId);
+              const updatedChat = {
+                ...chatThatMessageBelongsTo,
+                last_message: message,
+              };
+              setMyChats(
+                [updatedChat, ...filteredChats].sort((a, b) => a.id - b.id),
+              );
+            }}
+          />
+        </div>
       )}
 
       <div className="chat-container">
